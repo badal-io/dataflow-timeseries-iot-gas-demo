02ee44248394b0adf6e1cddbda4cc5e39f205d44
Lamine Diop <lamine.diop@axionable.com>
2022-08-19 17:25:24 +0200

    [Fix] Comment network setup (implemented in terraform)

--- terraform/scripts/setup_gcp_infra.sh
+++ terraform/scripts/setup_gcp_infra.sh
@@ -14,10 +14,10 @@ echo "=== Configuring terraform state repo ==="
 BUCKET_FOR_STATE="gs://$PROJECT_ID-terraform-state"
 gsutil mb -c regional -l us-central1 "$BUCKET_FOR_STATE" || true
 
-echo "=== Creating default compute network ==="
-gcloud compute networks create default
-gcloud compute firewall-rules create default-allow-icmp --network default --allow icmp --source-ranges 0.0.0.0/0
-gcloud compute firewall-rules create default-allow-ssh --network default --allow tcp:22 --source-ranges 0.0.0.0/0
+# echo "=== Creating default compute network ==="
+# gcloud compute networks create default
+# gcloud compute firewall-rules create default-allow-icmp --network default --allow icmp --source-ranges 0.0.0.0/0
+# gcloud compute firewall-rules create default-allow-ssh --network default --allow tcp:22 --source-ranges 0.0.0.0/0
 
 echo "=== Executing terraform scripts ==="
 #to be in terraform folder
